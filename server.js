4c85282bb7911fd7ff1fb8e8e6df7692a6650fa5
Shawn M. Crowley <scrowley@buffalo.edu>
2025-11-16 07:58:07 -0500

    Updated Server

--- server.js
+++ server.js
@@ -8,12 +8,16 @@ const HOST = "localhost";
 const PORT = 8020;
 
 app.use(express.static('public'));
+
+// Get Route for Main Dashboard Page
+
 app.get('/data', (req, res) => {
     console.log("Received request for /data");
     const htmlContent = `<p>This content was loaded from the server via HTMX using GET Method!</p><p><a href="/">Back to Dashboard</a></p>`;
     res.send(htmlContent);
 });
  
+// Post Route for Contact Form Submission
 
 app.post('/contacts', (req, res) => {
     // Process form submission, save data
@@ -21,6 +25,23 @@ app.post('/contacts', (req, res) => {
     // Return HTML for the new contact list item
     res.send(`<li>${newContactName}</li>`);
 });
+
+/**
+   * Health check endpoint `/health`
+   * 
+   * @path {HOST}:{PORT}/health
+   * @return status {200:OK}
+   * @return uptime : how long has been server up & running
+   * @return timestamp : Time of response from server  
+   */
+  app.get('/health', (req, res) =>{
+    const healthcheck = {
+      uptime: process.uptime(),
+      message: "OK",
+      timestamp: Date.now()}
+      res.send(JSON.stringify(healthcheck));
+  }); 
+
 
   // Start the server
   app.listen(PORT, () => {
